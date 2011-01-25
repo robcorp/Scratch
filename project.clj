5f20074a46ce5e3ca45e8555066e2f7eead6ed45
Robert C. Houser <robcorp@mac.com>
2011-01-24 20:43:53 -0500

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject scratch "1.0.0-SNAPSHOT"
+  :description "Useful for launching a repl to play around in."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
